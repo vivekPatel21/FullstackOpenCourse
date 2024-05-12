beb378b6d193c866a65f5094ecc29ab13b0abd0b
vivekPatel21 <vpatel20001@gmail.com>
2024-05-12 18:18:55 -0400

    1.1 complete

--- part1/excersizes/part1/src/App.jsx
+++ part1/excersizes/part1/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
+  console.log(props)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
   )
 }
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>{props.courseName} {props.assignmentNumber}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content courseName={part1} assignmentNumber={exercises1} />
+      <Content courseName={part2} assignmentNumber={exercises2} />
+      <Content courseName={part3} assignmentNumber={exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
 
 export default App
