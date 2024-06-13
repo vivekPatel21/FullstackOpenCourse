e5567b41647db4ee4aa565b53f278e4a9c9f9b92
Vivek Patel <vpatel20001@gmail.com>
2024-06-12 22:42:17 -0400

    importing frontend from part2cw

---
+++ part3/part3cw/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Note from './Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App 
